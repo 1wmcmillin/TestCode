b1dea9e45766d910256d47cf1b609608ed5452f0
1wmcmillin <42749672+1wmcmillin@users.noreply.github.com>
2018-08-30 13:58:56 -0500

    Add files via upload

---
+++ vars.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = -25;
+//create two more variables c and d and give them whatever number you want.
+let c = -49;
+let d = 99;
+
+const total = a+b+c+d;
+console.log(total);
+
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name; "William McMillin"
+//add string convo and name together.
+const greeting = "convo + name"
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2;
+let
+const
+console.log();
